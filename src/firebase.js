0751a319de245836e9efd706a5331ee3a7f85cdd
Giacomo McNabb <jack@Giacomos-MacBook-Pro.local>
2020-11-01 22:01:01 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAs5U0wQqhC-TxX3zg0sHSCJVOOKH_SHKM",
+  authDomain: "coindexter2.firebaseapp.com",
+  databaseURL: "https://coindexter2.firebaseio.com",
+  projectId: "coindexter2",
+  storageBucket: "coindexter2.appspot.com",
+  messagingSenderId: "234040119251",
+  appId: "1:234040119251:web:f81521c9e17a19a63fe926",
+  measurementId: "G-N0D3XT5V1B",
+};
+
+export default firebase;
