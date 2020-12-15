45d08b21c5f5f72cbf0557f4bc9cb225a043d7c2
James McNabb <jack.mcnabb@gmail.com>
2020-12-15 14:32:09 -0500

    added ClaimsPrincipal

--- coinStackAPI/Function1.cs
+++ coinStackAPI/Function1.cs
@@ -7,6 +7,11 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Text;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Linq;
 
 namespace coinStackAPI
 {
@@ -31,8 +36,9 @@ namespace coinStackAPI
 
             //return new OkObjectResult(responseMessage);
 
-            //string responseMessage = "successful API auth test";
-            string responseMessage = req.Headers.Values.ToString();
+            ClaimsPrincipal UserClaims = StaticWebAppsAuth.Parse(req);
+            string responseMessage = UserClaims.Identity.ToString();
+
             return new OkObjectResult(responseMessage);
         }
 
@@ -43,5 +49,46 @@ namespace coinStackAPI
         //    string responseMessage = "you an MVP";
         //    return new OkObjectResult(responseMessage);
         //}
+
+        public static class StaticWebAppsAuth
+        {
+            public class ClientPrincipal
+            {
+                public string IdentityProvider { get; set; }
+                public string UserId { get; set; }
+                public string UserDetails { get; set; }
+                public IEnumerable<string> UserRoles { get; set; }
+            }
+
+            public static ClaimsPrincipal Parse(HttpRequest req)
+            {
+                var principal = new ClientPrincipal();
+
+                if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
+                {
+                    var data = header[0];
+                    var decoded = Convert.FromBase64String(data);
+                    var json = Encoding.ASCII.GetString(decoded);
+                    principal = System.Text.Json.JsonSerializer.Deserialize<ClientPrincipal>
+                        (json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                }
+
+                principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" },
+                    StringComparer.CurrentCultureIgnoreCase);
+
+                if (!principal.UserRoles?.Any() ?? true)
+                {
+                    return new ClaimsPrincipal();
+                }
+
+                var identity = new ClaimsIdentity(principal.IdentityProvider);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+                identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+                identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+                return new ClaimsPrincipal(identity);
+            }
+        }
+
     }
 }
